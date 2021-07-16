e4e39edf771c86479635736a8debaeb6265a9dda
kaifeng25 <kaifengya1616@163.com>
2021-07-16 16:31:45 +0800

    press

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,29 +1,24 @@
 module.exports = {
     head: [
-        ['link', {rel: 'icon',href:'/favicon.ico'} ]
+        ['link', {
+            rel: 'icon',
+            href: '/favicon.ico'
+        }]
     ],
+    title: 'Kaite',
+    description: 'kaifeng的小站',
+    dest: './dist',
+    markdown: {
+        lineNumbers: true
+    },
     themeConfig: {
-      lastUpdated: '跟新时间',
-      logo: '/assets/img/hero.png',
-    //   navbar:false,
-      displayAllHeaders: true, // 默认值：false
-      sidebar: 'auto',
-      
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: 'About', link: '/about' },
-        { text: 'External', link: 'https://baidu.com' },
-        {
-            text: 'Languages',
-            ariaLabel: 'Language Menu',
-            items: [
-              { text: 'Chinese', link: '/language/chinese/' },
-              { text: 'Japanese', link: '/language/japanese/' }
-            ]
-          }
-      ],
-     
-    
-      
+        lastUpdated: '跟新时间',
+        logo: '/assets/img/hero.png',
+        //   navbar:false,
+        displayAllHeaders: true, // 默认值：false
+        nav: require("./nav"),
+        sidebar: require("./sidebar"),
+        editLinks: true,
+        editLinkText: '在 GitHub 上编辑此页 ！',
     }
-  }
+}
